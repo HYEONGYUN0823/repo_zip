627667506730b68bf431cf5fac8d43ecff6fe6f7
HYEONGYUN0823 <shiningin00@naver.com>
2025-03-24 19:30:34 +0900

    first

--- zip/src/main/java/com/a7a7/modeule/member/MemberController.java
+++ zip/src/main/java/com/a7a7/modeule/member/MemberController.java
@@ -1,15 +1,22 @@
 package com.a7a7.modeule.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MemberController {
-
-	@RequestMapping(value = "/xdm/member/MemberXdmList")
-	public String MemberXdmList() {
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping(value = "/xdm/member/MemberList")
+	public String memberXdmList() {
 		
-		return "xdm/member/MemberXdmList";
+		return "xdm/member/MemberList";
 	}
 	
+
 }
