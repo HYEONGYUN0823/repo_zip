b599477f2fc8954f6ddfd68f182111e9ddf118b2
HYEONGYUN0823 <shiningin00@naver.com>
2025-03-26 01:09:33 +0900

    first

--- zip/src/main/java/com/a7a7/modeule/member/MemberDto.java
+++ zip/src/main/java/com/a7a7/modeule/member/MemberDto.java
@@ -3,54 +3,110 @@ package com.a7a7.modeule.member;
 public class MemberDto {
 	
 	
-	private String ifcgSeq;			// 시퀀스
-	private String ifcgName;		// 코드그룹 이름
-	private String ifcgDelNy;		// 사용여부
-	private String ifcgUseNy;
+	private String seq;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String btd;
+	private String email;
+	private String passWord;
+	private String price;
+	private String score;
+	private String delNy;
 
 	private String xifcdSeqCount;	// 갯수
-	
-//	-----
-	
-	public String getIfcgSeq() {
-		return ifcgSeq;
-	}
-	
-	public void setIfcgSeq(String ifcgSeq) {
-		this.ifcgSeq = ifcgSeq;
-	}
-	
-	public String getIfcgName() {
-		return ifcgName;
-	}
-	
-	public void setIfcgName(String ifcgName) {
-		this.ifcgName = ifcgName;
-	}
-	
-	public String getIfcgDelNy() {
-		return ifcgDelNy;
-	}
-	
-	public String getIfcgUseNy() {
-		return ifcgUseNy;
+
+	public String getSeq() {
+		return seq;
 	}
 
-	public void setIfcgUseNy(String ifcgUseNy) {
-		this.ifcgUseNy = ifcgUseNy;
+	public void setSeq(String seq) {
+		this.seq = seq;
 	}
 
-	public void setIfcgDelNy(String ifcgDelNy) {
-		this.ifcgDelNy = ifcgDelNy;
+	public String getFirstName() {
+		return firstName;
 	}
-	
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getBtd() {
+		return btd;
+	}
+
+	public void setBtd(String btd) {
+		this.btd = btd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+	public String getDelNy() {
+		return delNy;
+	}
+
+	public void setDelNy(String delNy) {
+		this.delNy = delNy;
+	}
+
 	public String getXifcdSeqCount() {
 		return xifcdSeqCount;
 	}
-	
+
 	public void setXifcdSeqCount(String xifcdSeqCount) {
 		this.xifcdSeqCount = xifcdSeqCount;
 	}
+	
+//	-----
+	
+	
 	
 	
 }
