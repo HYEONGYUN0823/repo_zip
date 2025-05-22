c05fd27bafae0d2963dd9b8b422ba1a085b28628
HYEONGYUN0823 <shiningin00@naver.com>
2025-05-22 15:26:44 +0900

    first

--- zip/src/main/java/com/a7a7/modeule/code/CodeController.java
+++ zip/src/main/java/com/a7a7/modeule/code/CodeController.java
@@ -1,5 +1,14 @@
 package com.a7a7.modeule.code;
 
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.a7a7.common.util.UtilDateTime;
 import com.a7a7.modeule.codegroup.CodeGroupService;
 import com.a7a7.modeule.codegroup.CodeGroupVo;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -28,6 +37,10 @@ public class CodeController {
 	@RequestMapping(value = "/xdm/code/CodeXdmList")
 	public String codeXdmList(@ModelAttribute("vo") CodeVo vo, Model model) throws Exception {
 		setSearch(vo); // 검색 조건 설정
+	    // rowNumToShow 기본값 처리
+	    if (vo.getRowNumToShow() <= 0) {
+	        vo.setRowNumToShow(5);
+	    }
 		vo.setParamsPaging(codeService.selectOneCount(vo));
 		
 
@@ -82,5 +95,107 @@ public class CodeController {
 		return "redirect:/xdm/code/CodeXdmList";
 	}
 	
+	@RequestMapping("/excelDownload")
+    public void excelDownload(CodeVo vo, HttpServletResponse httpServletResponse) throws Exception {
+	    
+		setSearch(vo);
+		vo.setParamsPaging(codeService.selectOneCount(vo));
+
+		if (vo.getTotalRows() > 0) {
+			List<CodeDto> list = codeService.selectList(vo);
+			
+			Workbook workbook = new HSSFWorkbook();	// for xls
+//	        Workbook workbook = new XSSFWorkbook();
+	        Sheet sheet = workbook.createSheet("첫번째 시트");
+	        CellStyle cellStyle = workbook.createCellStyle();        
+	        Row row = null;
+	        Cell cell = null;
+	        int rowNum = 0;
+			
+//	        each column width setting
+	        sheet.setColumnWidth(0, 2100);
+	        sheet.setColumnWidth(1, 3100);
+
+//	        Header
+	        String[] tableHeader = {"사용", "코드그룹 코드", "코드그룹 이름", "코드 이름", "삭제", "등록일", "수정일"};
+
+	        row = sheet.createRow(rowNum++);
+			for(int i=0; i<tableHeader.length; i++) {
+				cell = row.createCell(i);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+				cell.setCellValue(tableHeader[i]);
+			}
+
+//	        Body
+			for (int i = 0; i < list.size(); i++) {
+			    row = sheet.createRow(rowNum++);
+
+			    // 0. 사용 여부
+			    cell = row.createCell(0);
+			    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			    cell.setCellStyle(cellStyle);
+			    if (list.get(i).getIfcdUseNy() != null) {
+			        cell.setCellValue("0".equals(list.get(i).getIfcdUseNy()) ? "N" : "Y");
+			    }
+
+			    // 1. 코드그룹 코드
+			    cell = row.createCell(1);
+			    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			    cell.setCellStyle(cellStyle);
+			    cell.setCellValue(Integer.parseInt(list.get(i).getIfcgSeq()));
+
+			    // 2. 코드그룹 이름
+			    cell = row.createCell(2);
+			    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			    cell.setCellStyle(cellStyle);
+			    cell.setCellValue(list.get(i).getIfcgName());
+
+			    // 3. 코드 이름
+			    cell = row.createCell(3);
+			    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			    cell.setCellStyle(cellStyle);
+			    cell.setCellValue(list.get(i).getIfcdName());
+
+			    // 4. 삭제 여부
+			    cell = row.createCell(4);
+			    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			    cell.setCellStyle(cellStyle);
+			    if (list.get(i).getIfcdDelNy() != null) {
+			        cell.setCellValue("0".equals(list.get(i).getIfcdDelNy()) ? "N" : "Y");
+			    }
+
+			    // 5. 등록일
+			    cell = row.createCell(5);
+			    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			    cell.setCellStyle(cellStyle);
+			    if (list.get(i).getIfcdRegDateTime() != null) {
+			    	cell.setCellValue(list.get(i).getIfcdRegDateTime());
+			    }
+
+			    // 6. 수정일
+			    cell = row.createCell(6);
+			    cellStyle.setAlignment(HorizontalAlignment.CENTER);
+			    cell.setCellStyle(cellStyle);
+			    if (list.get(i).getIfcdModDateTime() != null) {
+			        cell.setCellValue(list.get(i).getIfcdModDateTime());
+			    }
+			}
+
+	        httpServletResponse.setContentType("ms-vnd/excel");
+	        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=example.xls");	// for xls
+//	        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=example.xlsx");
+
+	        workbook.write(httpServletResponse.getOutputStream());
+	        workbook.close();
+		}
+    }
+
+
+
+
+
+
+
 	
 }
